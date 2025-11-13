375ff9e915508a1f054fc3cfa020e2e391fbc9e9
Amay-LG <128310420+Amay-LG@users.noreply.github.com>
2025-11-12 16:12:00 -0800

    Update Timer.cpp comments

--- Timer.cpp
+++ Timer.cpp
@@ -9,7 +9,11 @@
 
 
 Timer * Timer::sInstance = NULL;
-
+/*
+======================================
+Initialize a Timer Instance
+======================================
+*/
 Timer * Timer::Instance() {
 
     if(sInstance == NULL){
@@ -19,39 +23,83 @@ Timer * Timer::Instance() {
     return sInstance;
 }
 
+/*
+======================================
+Release a Timer Instance
+======================================
+*/
 void Timer::Release() {
 
     delete sInstance;
     sInstance = NULL;
 }
 
+/*
+======================================
+Constructor for Timer
+======================================
+*/
 Timer::Timer() {
     Reset();
     mTimeScale = 1.0f;
 }
 
+/*
+======================================
+Destructor for Timer
+======================================
+*/
 Timer::~Timer() {
 
 }
-
+/*
+======================================
+Resets Timer values
+======================================
+*/
 void Timer::Reset() {
     mStartTicks = SDL_GetTicks();
     mElapsedTicks = 0;
     mDeltaTime = 0.0f;
 }
-
+/*
+======================================
+Returns the time elapsed in seconds
+======================================
+*/
 float Timer::DeltaTime() {
     return mDeltaTime;
 }
 
+/*
+======================================
+Setter method for mTimeScale
+
+mTimeScale is used to handle time-based movement
+mTimeScale can be used to update game speed in future iterations
+
+Parameters:
+>> t: value to set to mTimeScale
+======================================
+*/
 void Timer::TimeScale(float t) {
     mTimeScale = t;
 }
-
+/*
+======================================
+Getter method for mTimeScale
+======================================
+*/
 float Timer::TimeScale() {
     return mTimeScale;
 }
 
+/*
+======================================
+Update Timer, store time elapsed in ticks in mElapsedTicks
+store time elapsed in seconds in mDeltaTime
+======================================
+*/
 void Timer::Update() {
     mElapsedTicks = SDL_GetTicks() - mStartTicks;
     mDeltaTime = mElapsedTicks * 0.001f; //Converts Ticks(ms) to seconds
